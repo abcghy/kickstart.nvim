11b5f7773aa72b3618b2f9970abec65cd9e67afc
Harry Gao <sakura@Harrys-Mac-mini.lan>
2023-07-24 00:10:03 +0800

    update nvimtree config

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,6 +1,20 @@
 vim.keymap.set('n', 'tt', '<cmd>NvimTreeToggle<cr>', { desc = '[T]ree [T]oggle'})
 vim.keymap.set('n', 'tf', '<cmd>NvimTreeFocus<cr>', { desc = '[T]ree [F]ocus' })
 
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+
+end
+
 return {
   'kyazdani42/nvim-tree.lua',
   dependencies = {
@@ -9,6 +23,7 @@ return {
   config = function ()
     require('nvim-tree').setup({
       hijack_cursor = true,
+      on_attach = on_attach,
       view = {
           width = 50,
           mappings = {
